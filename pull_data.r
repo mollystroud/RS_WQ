e7d818e9bb65cda6c8e6da8f8c84efc5cff19b7b
mollystroud <mollykstroud@gmail.com>
2025-10-02 14:43:06 -0400

    Fixed Otsu thresholding so each image's thresh is calculated separately

--- pull_data.r
+++ pull_data.r
@@ -10,30 +10,54 @@ p_load('rmarkdown','earthdatalogin', 'rstac','imager','lubridate','xts',
 ## the below code is designed to pull HLS imagery over a specified area and 
 # mask out any land
 ################################################################################
-# define endpoint
+# define stac url
 s = stac("https://cmr.earthdata.nasa.gov/stac/LPCLOUD/")
 
-# we want both sentinel and landsat hls data
+# both sentinel and landsat hls data
 HLS_col <- list("HLSS30_2.0", "HLSL30_2.0")
 
 # beaverdam reservoir bbox
-bdr_box <- c(xmin = -79.827088, ymin = 37.311798, xmax = -79.811865, ymax = 37.321694)
+bdr_box <- c(xmin = -79.827088, 
+             ymin = 37.311798, 
+             xmax = -79.811865, 
+             ymax = 37.321694)
 # falling creek reservoir bbox
-fcr_box <- c(xmin = -79.840037, ymin = 37.301435, xmax = -79.833651, ymax = 37.311487)
+fcr_box <- c(xmin = -79.840037, 
+             ymin = 37.301435, 
+             xmax = -79.833651, 
+             ymax = 37.311487)
 # carvins cove reservoir bbox
-ccr_box <- c(xmin = -79.981728, ymin = 37.367542, xmax = -79.942552, ymax = 37.407255)
-ccr_box_wgs84 <- sf::st_bbox(c(xmin = -79.981728, ymin = 37.367542, 
-                               xmax = -79.942552, ymax = 37.407255), 
-                             crs = "EPSG:4326")
+ccr_box <- c(xmin = -79.981728, 
+             ymin = 37.367542, 
+             xmax = -79.942552, 
+             ymax = 37.407255)
 
-# Convert the bounding boxes to the correct UTM projection
+# convert the bounding boxes to the correct UTM projection
+bdr_box_utm <- sf::st_bbox(
+  sf::st_transform(sf::st_as_sfc(sf::st_bbox(c(xmin = -79.827088, 
+                                               ymin = 37.311798, 
+                                               xmax = -79.811865, 
+                                               ymax = 37.321694), 
+                                             crs = "EPSG:4326")), "EPSG:32617")
+)
+fcr_box_utm <- sf::st_bbox(
+  sf::st_transform(sf::st_as_sfc(sf::st_bbox(c(xmin = -79.840037, 
+                                               ymin = 37.301435, 
+                                               xmax = -79.833651, 
+                                               ymax = 37.311487), 
+                                             crs = "EPSG:4326")), "EPSG:32617")
+)
 ccr_box_utm <- sf::st_bbox(
-  sf::st_transform(sf::st_as_sfc(ccr_box_wgs84), "EPSG:32617")
+  sf::st_transform(sf::st_as_sfc(sf::st_bbox(c(xmin = -79.981728, 
+                                               ymin = 37.367542, 
+                                               xmax = -79.942552, 
+                                               ymax = 37.407255), 
+                                             crs = "EPSG:4326")), "EPSG:32617")
 )
 
 # set date of interest (will need to change this later to update every day)
-start_date <- "2024-07-01T00:00:00Z"
-end_date <- "2024-07-30T00:00:00Z"
+start_date <- "2025-09-01T00:00:00Z"
+end_date <- "2025-09-30T00:00:00Z"
 
 # grab items within dates of interest
 items <- s %>%
@@ -134,21 +158,49 @@ clean_mndwi_stars <- do.call(c, c(slices, along = "time"))
 tm_shape(shp = clean_mndwi_stars) + 
   tm_raster(col = "mNDWI")
 
-# identify water using the Otsu thresholding method (https://ieeexplore.ieee.org/document/4310076)
-vals <- as.vector(clean_mndwi_stars[[1]])
-# scale to 0 to 1 so we can apply Otsu thresholding
-vals_scaled <- (vals - min(vals, na.rm = TRUE)) / (max(vals, na.rm = TRUE) - min(vals, na.rm = TRUE))
-# calculate the Otsu threshold 
-threshold <- otsu(matrix(vals_scaled, ncol = 1)) # use otsu to threshold
-# threshold the image data to create a binary mask
-binary_mask_matrix <- (clean_mndwi_stars[[1]] > threshold)
-binary_stars <- st_as_stars(list(star_mask = binary_mask_matrix),
-                            dimensions = st_dimensions(clean_stars))
+# identify water using the Otsu thresholding method for each image 
+# (https://ieeexplore.ieee.org/document/4310076)
+n_dates <- dim(clean_mndwi_stars)["time"] # get dates
+time_labels <- st_get_dimension_values(clean_mndwi_stars, "time")
 
+# get spatial dims
+nx <- dim(clean_mndwi_stars)["x"]
+ny <- dim(clean_mndwi_stars)["y"]
+
+# set up for loop
+mask_array <- array(NA, dim = c(nx, ny, n_dates))
+thresholds <- numeric(n_dates)
+# for each date, scale mndwi values (0-1) and calculate Otsu threshold
+for (i in 1:n_dates) {
+  slice <- clean_mndwi_stars[,,,i, drop = TRUE]
+  vals <- as.vector(slice[[1]])
+  
+  vals_scaled <- (vals - min(vals, na.rm = TRUE)) / (max(vals, na.rm = TRUE) - min(vals, na.rm = TRUE))
+  threshold_i <- otsu(matrix(vals_scaled, ncol = 1))
+  thresholds[i] <- threshold_i
+  
+  mask_array[,,i] <- slice[[1]] > threshold_i
+}
+
+# convert array back to stars
+binary_stars <- st_as_stars(mask_array)
+
+# attach dimensions (x, y from original, time from labels)
+binary_stars <- st_set_dimensions(binary_stars, "x", 
+                                  values = st_get_dimension_values(clean_mndwi_stars, "x"))
+binary_stars <- st_set_dimensions(binary_stars, "y", 
+                                  values = st_get_dimension_values(clean_mndwi_stars, "y"))
+binary_stars <- st_set_dimensions(binary_stars, "time", 
+                                  values = time_labels)
+
+names(binary_stars) <- "watermask"
 
 # viz
 tm_shape(shp = binary_stars) + 
-  tm_raster(col = "star_mask")
+  tm_raster(col = "watermask")
+
+
+
 
 
 test <- as(clean_stars, "Raster")
@@ -159,113 +211,4 @@ plot(test_mask)
 test_masked <- terra::mask(test$layer, test_mask$layer)
 
 plot(test_masked)
-
-
-################################################################################
-# with rast() instead of gdalcubes
-################################################################################
-
-# place items in spatial df
-sf_items <- items_as_sf(items)
-
-# retrieve granule ID for each feature
-granule_id <- sapply(items$features, function(feature) feature$id)
-
-# add as first column in sf_items
-sf_items <- cbind(granule = granule_id, sf_items)
-
-# set up function to grab bands of interest for both HLSS and HLSL
-extract_asset_urls <- function(feature) {
-  collection_id <- feature$collection
-  if (collection_id == "HLSS30_2.0") {
-    bands = c('B8A','B04','B03', 'B11', 'Fmask')
-  } else if (collection_id == "HLSL30_2.0") {
-    bands = c('B05','B04', 'B03', 'B11', 'Fmask')}
-  sapply(bands, function(band) feature$assets[[band]]$href)
-}
-
-
-# retrieve asset URLs for each feature using extract_asset_urls function and transpose them to columns
-asset_urls <- t(sapply(items$features, extract_asset_urls))
-colnames(asset_urls) <- c('nir', 'red', 'green', 'SWIR', 'fmask') # clean up column names
-
-
-sf_items <- cbind(sf_items, asset_urls)
-
-# reset row indices
-row.names(sf_items) <- NULL
-
-## 
-setGDALconfig("GDAL_HTTP_UNSAFESSL", value = "YES")
-setGDALconfig("GDAL_HTTP_COOKIEFILE", value = ".rcookies")
-setGDALconfig("GDAL_HTTP_COOKIEJAR", value = ".rcookies")
-setGDALconfig("GDAL_DISABLE_READDIR_ON_OPEN", value = "EMPTY_DIR")
-setGDALconfig("CPL_VSIL_CURL_ALLOWED_EXTENSIONS", value = "TIF")
-
-# read an HLS scene from URL, apply the scale factor if necessary, and optionally crops and
-# masks the scene based on a polygon. Requery the above GDAL configurations.
-open_hls <- function(url, roi = NULL) {
-  # Add VSICURL prefix
-  url <- paste0('/vsicurl/', url)
-  # Retrieve metadata
-  meta <- describe(url)
-  # Check if dataset is Quality Layer (Fmask) - no scaling this asset (int8 datatype)
-  is_fmask <- any(grep("Fmask", meta))
-  # Check if Scale is present in band metadata
-  will_autoscale <- any(grep("Scale:", meta))
-  # Read the raster
-  r <- rast(url)
-  # Apply Scale Factor if necessary
-  if (!will_autoscale && !is_fmask){
-    print(paste("No scale factor found in band metadata. Applying scale factor of 0.0001 to", basename(url)))
-    r <- r * 0.0001
-  }
-  # Crop if roi specified
-  if (!is.null(roi)){
-    # project to degrees
-    r_reproj <- project(r, "epsg:4326")
-    # crop to roi
-    e <- terra::ext(roi[1], roi[3], roi[2], roi[4])
-    r <- terra::crop(r_reproj, e)
-  }
-  return(r)
-}
-
-swir <- open_hls(sf_items$SWIR[1], ccr_box)
-
-ggplot() + 
-  geom_spatraster(data = swir) +
-  scale_fill_viridis() +
-  theme_classic() 
-
-green <- open_hls(sf_items$green[1], ccr_box)
-
-ndwi <- (green - swir) / (green + swir)
-
-ggplot() + 
-  geom_spatraster(data = ndwi) +
-  scale_fill_viridis() +
-  theme_classic() 
-# identify water using the Otsu thresholding method
-vals <- values(ndwi, mat = F) # get ndwi values
-vals <- vals[!is.na(vals)] # remove any nas
-vals_scaled <- (vals - min(vals, na.rm = TRUE)) / (max(vals, na.rm = TRUE) - min(vals, na.rm = TRUE))
-threshold <- otsu(matrix(vals_scaled, ncol = 1)) # use otsu to threshold
-water_mask <- ndwi > threshold # identify values over threshold
-#classify and plot
-water_mask01 <- classify(water_mask, rcl = matrix(c(0, 0, 1, 1), ncol = 2, byrow = TRUE))
-plot(water_mask01, main = "Water Mask (Otsu Threshold)")
-plot(water_mask)
-ggplot() + 
-  geom_spatraster(data = water_mask) +
-  scale_fill_viridis() +
-  theme_classic() 
-
-# extract value at location
-location <- data.frame(x = 37.315348, y = -79.819365)
-pt <- data.frame(lon = -79.819365, lat = 37.315348)
-nir_val <- terra::extract(nir_reproj, pt)
-
-
-
 
